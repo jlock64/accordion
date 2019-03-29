a9cfbc313484d64c0e8934fc9e63a9c3b9d59184
Justin Lock <jlock@TRHC.com>
2019-03-28 22:34:51 -0400

    add accordion functionality and final styling touches

--- public/js/main.js
+++ public/js/main.js
@@ -5,13 +5,35 @@ document.addEventListener('click', (event) => {
   console.log('target: ', event.target)
   
   console.log('classList: ', event.target.classList.contains("accordion-toggle"))
-
+  
   // Bail if our clicked element doesn't have the .accordion-toggle class
   if (!event.target.classList.contains("accordion-toggle")) {
     return;
   }
   
-  // Log the clicked element in the console
-  console.log('matches');
+  // Get the target content
+  var content = document.querySelector((event.target.hash))
+  
+  // If no content is found we will end the function
+  if (!content) return
+  
+  // Prevent default link behavior
+  event.preventDefault();
+  
+  // If the content is already expanded, collapse it and quit
+  if (content.classList.contains('active')) {
+    content.classList.remove('active')
+    return
+  }
+  
+  // Get all open accordion content, loop through it, and close it
+  var accordions = document.querySelectorAll('.accordion-content.active')
+  console.log('accordions: ', accordions)
+  accordions.forEach(function (accordion) {
+    accordion.classList.remove('active')
+  })
+  
+  // Toggle our content
+  content.classList.toggle('active')
   
 })
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const serveIndex = require('serve-index');
 const port = 3000
 
 // access static files here like css, js and images
-app.use('/public', express.static('public'));
+app.use('/public', express.static('public'))
+app.use('/gators', express.static('public'))
+app.use('/gators', serveIndex('public'))
 
 // routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
